b6b553b19b8cc188eb8733f58591f1bcd7d3a1dc
server <Truvam@users.noreply.github.com>
2021-02-04 12:39:56 +0000

    Fixed gst server stopping when a client disconnects

--- server/WebRTC/gst/entrypoint.sh
+++ server/WebRTC/gst/entrypoint.sh
@@ -4,9 +4,12 @@ FRAMERATE=30
 AUDIO=false
 CLIPBOARD=false
 
-python3 /opt/app/main.py --debug \
-    --app_name $APP_NAME \
-    --framerate $FRAMERATE \
-    --enable_audio $AUDIO \
-    --enable_clipboard $CLIPBOARD \
-    --encoder $ENCODER
+while true; do
+    python3 /opt/app/main.py --debug \
+        --app_name $APP_NAME \
+        --framerate $FRAMERATE \
+        --enable_audio $AUDIO \
+        --enable_clipboard $CLIPBOARD \
+        --encoder $ENCODER
+    sleep 1
+done
