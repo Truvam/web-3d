9cf89b62586aa05dffb72159ac984dc46d9abfa2
server <Truvam@users.noreply.github.com>
2021-02-24 17:06:24 +0000

    Fixed wrong default encoder

--- scripts/gst-webrtc-app.sh
+++ scripts/gst-webrtc-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting GStreamer WebRTC Application."
-sudo docker run -it --rm --gpus all --network=host -e ENCODER=x264enc -e APP_NAME=WebViewer -e SERVER= server-gst
+sudo docker run -it --rm --gpus all --network=host -e ENCODER=nvh264enc -e APP_NAME=WebViewer -e SERVER= server-gst
 
