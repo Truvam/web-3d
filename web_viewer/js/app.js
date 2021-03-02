0844bf9cc66976d0f6f09d1b2722b447ab8c18cb
server <Truvam@users.noreply.github.com>
2021-03-02 12:35:33 +0000

    Added method and buttons to call the asset loader

--- web_viewer/js/app.js
+++ web_viewer/js/app.js
@@ -4,6 +4,25 @@ var app = new Vue({
     vuetify: new Vuetify(),
 
     data: {
-      
+        assets: [
+            { url: 'example/fbx/22-rp_manuel_animated_001_dancing_fbx/rp_manuel_animated_001_dancing.fbx', value: 0 },
+            { url: 'example/fbx/Samba Dancing.fbx', value: 1 }
+        ]
+    },
+
+    components: {
+    },
+
+    methods: {
+        loadAsset(assetNumber) {
+
+            console.log("Loading Asset: " + assetNumber);
+            this.$loader(app.assets[assetNumber].url);
+
+        }
+    },
+
+    watch: {
     }
-  })
+})
+
