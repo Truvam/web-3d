83c3d7d17f02ae12b9e56e88bbb9ff9b2a4e9873
server <Truvam@users.noreply.github.com>
2021-03-03 11:11:59 +0000

    Added switcher to show and hide UI

--- web_viewer/js/app.js
+++ web_viewer/js/app.js
@@ -7,7 +7,10 @@ var app = new Vue({
         assets: [
             { url: 'example/fbx/22-rp_manuel_animated_001_dancing_fbx/rp_manuel_animated_001_dancing.fbx', value: 0 },
             { url: 'example/fbx/Samba Dancing.fbx', value: 1 }
-        ]
+        ],
+
+        showToolbar: false,
+        showUI: true
     },
 
     components: {
